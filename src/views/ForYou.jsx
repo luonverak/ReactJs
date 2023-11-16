28a067c153dc458005fc94f82901a2772b6b64f8
luonverak <112792376+luonverak@users.noreply.github.com>
2023-11-17 00:33:35 +0700

    code commit

--- src/views/ForYou.jsx
+++ src/views/ForYou.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import '../style/ForYou.css';
+const data=[];
+
 const ForYou=()=>{
+    for(let i=0;i<6;i++){
+        data[i]=<div className="item-foryou">
+                    <div className="thumbnail-item">
+
+                        <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63e8c4e54b76914b262f2448_headphone-min.png" alt=""/>
+                    </div>
+                    <div className="item-name">
+                        <h1>HomePod mini</h1>
+                        <h1>$239.00</h1>
+                    </div>
+                    <div className="item-detail">
+                        <p>
+                            Table with air purifier, stained veneer/blackTable with air purifier, stained veneer/blackTable with air purifier, stained veneer/blackTable with air purifier, stained veneer/black
+                        </p>
+                    </div>
+                    <div className="add-to-cart">
+                        <button type="button">Add to Cart</button>
+                    </div>
+
+                </div>
+    }
     return (
         <>
-            
+            <div className="foryou">
+                <div className="title-foryou">
+                    <h1>Todays Best Deals For You!</h1>
+                </div>
+                <div className="show-foryou">
+                    {data}
+                </div>
+
+            </div>
         </>
     );
 }
--- src/views/IndexPage.jsx
+++ src/views/IndexPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TopCategory from "./TopCategory";
 import SlideShow from './SlideShow';
+import ForYou from "./ForYou";
 const IndexPage=()=>{
     return (
         <>
             <SlideShow />
             <TopCategory/>
+            <ForYou/>
         </>
     );
 }
